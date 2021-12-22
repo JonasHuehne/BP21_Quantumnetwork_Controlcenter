5c2e7d3beaa18c531486abc240d3fb99123435b1
sashapetri <sasha.petri@stud.tu-darmstadt.de>
2021-12-22 14:07:49 +0100

    fixed merge issues caused by previous merge

---
+++ Quantumnetwork Controllcenter/src/frame/QuantumnetworkControllcenter.java
@@ -0,0 +1,35 @@
+package frame;
+
+/*
+ * Main Class of QuantumnetworkControllcenter
+ */
+public class QuantumnetworkControllcenter {
+	
+	
+	/*
+	 * Method to initialize a Quantumnetwork Controllcenter
+	 */
+	public static void initialize() {
+		
+		//TODO add initialization of further Classes
+		
+		System.out.println("QuantumnetworkControllcenter initialized");
+	}
+	 
+	
+	/*
+	 * Main-method to run QuantumnetworkControllcenter
+	 */
+	public static void main(String[] args) {
+		
+
+		
+		System.out.println("Run QuantumnetworkControllCenter initialisation");
+		
+		initialize();
+		
+		// switched workspace in Eclipse
+		
+	}
+	
+}
