8a94f05afa10ec5db70f625b301dd968a0c594c2
JonasHuehne <31365924+JonasHuehne@users.noreply.github.com>
2021-12-25 13:43:37 +0100

    Added ConMan Init

--- Quantumnetwork Controllcenter/src/frame/QuantumnetworkControllcenter.java
+++ Quantumnetwork Controllcenter/src/frame/QuantumnetworkControllcenter.java
@@ -1,17 +1,23 @@
 package frame;
 
+import networkConnection.ConnectionManager;
+
 /*
  * Main Class of QuantumnetworkControllcenter
  */
 public class QuantumnetworkControllcenter {
 	
-	
+	public static ConnectionManager conMan;
 	/*
 	 * Method to initialize a Quantumnetwork Controllcenter
 	 */
 	public static void initialize() {
 		
 		//TODO add initialization of further Classes
+		
+		//Network Connection Init
+		String localIP = "127.0.0.1"; //Must be changed manually as of right now. Use IP depending on intended communication Range (local Machine, local Network or Internet)
+		conMan = new ConnectionManager(localIP);
 		
 		System.out.println("QuantumnetworkControllcenter initialized");
 	}
