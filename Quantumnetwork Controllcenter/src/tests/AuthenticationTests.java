899509e0e919474ede3b15ef58e339a6a63abf2c
sarah <sarah@munin-soft.de>
2022-01-02 15:06:24 +0100

    add: additional test for verification

--- Quantumnetwork Controllcenter/src/tests/AuthenticationTests.java
+++ Quantumnetwork Controllcenter/src/tests/AuthenticationTests.java
@@ -16,7 +16,7 @@ public class AuthenticationTests {
     }
 
     @Test
-    // only testable, if testSign worked
+    // only testable, if signing works
     public void testVerifyTrue () {
         Database.insert("self", "127.0.0.0", 2303, Authentication.publicKeyString);
         String signature = Authentication.sign("Hello");
@@ -26,13 +26,33 @@ public class AuthenticationTests {
     }
 
     @Test
-    // only testable, if testSign worked
+    // only testable, if signing works
     public void testVerifyFalse () {
         Database.insert("self", "127.0.0.0", 2303, Authentication.publicKeyString);
         String signature = Authentication.sign("Hello");
         boolean result = Authentication.verify("Hallo", signature, "self");
         Assertions.assertFalse(result);
         Database.delete("self");
+    }
+
+    @Test
+    // only testable, if signing works
+    public void testVerifyFalse2 () {
+        String otherPublicKeyString =
+                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5r12pr0ZBtvFj133y9Yz" +
+                "UCmivnUycRU3T/TBFTiIV7Li7NN11RQ+RdOUzuNOB7A5tQIzkzNPJSOHC2ogxXnE" +
+                "yG6ClQS/YQ6hGQ4BH/FMz8h3HWsA/d9rhL1csmz8xJeqCoK0djEph1qGkso/AyoK" +
+                "LohV1zXgRM3EMV09ZgJAEktw6xxuzDtoLvDe7LMtYb/ahtdpYQMGSaHmUlEsC5Wk" +
+                "hbZkxGgs0LZD1Tjk9zGQ2bHbfU1wR7XhMku0riIxk32pNNJ+E2VSGIK5UJIyjbHM" +
+                "iX5wyzy+frpgvA4YyonXJJRs4dp6Jngy9BwYnCJjeHgcFdVtIqjYTEIcy3w4FsEX" +
+                "1QIDAQAB";
+        Database.insert("self", "127.0.0.0", 2303, Authentication.publicKeyString);
+        Database.insert("other", "128.0.0.0", 2505, otherPublicKeyString);
+        String signature = Authentication.sign("Hello");
+        boolean result = Authentication.verify("Hello", signature, "other");
+        Assertions.assertFalse(result);
+        Database.delete("self");
+        Database.delete("other");
     }
 
 }
