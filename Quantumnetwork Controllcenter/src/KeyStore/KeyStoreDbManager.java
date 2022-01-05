ad8094741864837076bc5a1f5c738a8f3eae594f
sarah <sarah@munin-soft.de>
2022-01-05 13:02:41 +0100

    changed: folder structure, deleted: files not needed in merge

--- Quantumnetwork Controllcenter/src/KeyStore/KeyStoreDbManager.java
+++ Quantumnetwork Controllcenter/src/KeyStore/KeyStoreDbManager.java
@@ -1,6 +1,6 @@
+package KeyStore;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
--- Quantumnetwork Controllcenter/src/KeyStore/KeyStoreObject.java
+++ Quantumnetwork Controllcenter/src/KeyStore/KeyStoreObject.java
@@ -1,3 +1,5 @@
+package KeyStore;
+
 public final class KeyStoreObject {
 
     /**
