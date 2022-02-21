6540e80ce4be689a5149cc4fe4a64b7cc969ca45
sarah <sarah@munin-soft.de>
2022-02-21 14:11:06 +0100

    add: method implementations for Configuration class

--- Quantumnetwork Controllcenter/src/main/frame/Configuration.java
+++ Quantumnetwork Controllcenter/src/main/frame/Configuration.java
@@ -1,10 +1,20 @@
 package frame;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Properties;
 
 public class Configuration {
+
+    /**
+     * The path to the folder for the program data, incl a file separator at the end
+     * Default path unless changed in the settings
+     */
+    private static String basePath = System.getProperty("user.dir") + File.separator;
 
     /**
      * Name of the properties file
@@ -12,11 +22,10 @@ public class Configuration {
     private static final String PROPERTIES_FILE_NAME = "properties.xml";
 
     /**
-     * The path to the folder for the property files, incl a file separator at the end
-     * Default path unless changed in the settings
+     * The path to the folder for the property files starting from the base path,
+     * incl a file separator at the end
      */
-    private static String propertiesPath = System.getProperty("user.dir")
-            + File.separator + "properties" + File.separator;
+    private static final String PROPERTIES_PATH = "properties" + File.separator;
 
     /**
      *
@@ -24,8 +33,26 @@ public class Configuration {
      * @param moveProperties
      * @return
      */
-    public static boolean changePath (String absolutePath, Boolean moveProperties) {
-        // TODO
+    public static boolean changeBasePath (String absolutePath, Boolean moveProperties) {
+        Properties properties;
+        if (moveProperties) {
+            properties = new Properties();
+            try {
+                // create an input stream
+                FileInputStream in = new FileInputStream
+                        (basePath + PROPERTIES_PATH + PROPERTIES_FILE_NAME);
+                // read the properties from file
+                properties.loadFromXML(in);
+                in.close();
+            } catch (Exception e) {
+                System.err.println("Error while changing the base path: " + e.getMessage());
+                return false;
+            }
+        } else {
+            properties = null;
+        }
+        basePath = absolutePath;
+        return createProperties(properties);
     }
 
     /**
@@ -33,14 +60,36 @@ public class Configuration {
      * @return
      */
     public static boolean createProperties () {
+        return createProperties(null);
+    }
+
+    /**
+     *
+     * @param prop
+     * @return
+     */
+    private static boolean createProperties(Properties prop) {
         try {
-            if (!Files.exists(Path.of(propertiesPath))) {
-                Files.createDirectory(Path.of(propertiesPath));
+            if (!Files.exists(Path.of(basePath + PROPERTIES_PATH))) {
+                Files.createDirectory(Path.of(basePath + PROPERTIES_PATH));
             }
-            // TODO
+            if (!Files.exists(Path.of(basePath + PROPERTIES_PATH + PROPERTIES_FILE_NAME))) {
+                // create an output stream
+                FileOutputStream out = new FileOutputStream
+                        (basePath + PROPERTIES_PATH + PROPERTIES_FILE_NAME);
+                // set the properties
+                Properties properties;
+                if (prop == null) {
+                     properties = new Properties();
+                } else {
+                    properties = prop;
+                }
+                properties.storeToXML(out, null, StandardCharsets.ISO_8859_1);
+                out.close();
+            }
             return true;
         } catch (Exception e) {
-            // TODO
+            System.err.println("Error while creating a properties file: " + e.getMessage());
             return false;
         }
     }
@@ -51,7 +100,24 @@ public class Configuration {
      * @return
      */
     public static String getProperty (String propertyKey) {
-        // TODO
+        if (Files.exists(Path.of(basePath + PROPERTIES_PATH + PROPERTIES_FILE_NAME))) {
+            try {
+                // create an input stream
+                FileInputStream in = new FileInputStream
+                        (basePath + PROPERTIES_PATH + PROPERTIES_FILE_NAME);
+                // read the properties from file
+                Properties properties = new Properties();
+                properties.loadFromXML(in);
+                in.close();
+                return properties.getProperty(propertyKey);
+            } catch (Exception e) {
+                System.err.println("Error while reading or returning a property: " + e.getMessage());
+                return null;
+            }
+        } else {
+            createProperties();
+            return null;
+        }
     }
 
     /**
@@ -61,6 +127,28 @@ public class Configuration {
      * @return
      */
     public static boolean setProperty (String propertyKey, String propertyValue) {
-        // TODO
+        try {
+            Properties properties = new Properties();
+            if (Files.exists(Path.of(basePath + PROPERTIES_PATH + PROPERTIES_FILE_NAME))) {
+                // create an input stream
+                FileInputStream in = new FileInputStream
+                        (basePath + PROPERTIES_PATH + PROPERTIES_FILE_NAME);
+                // read the properties from file
+                properties.loadFromXML(in);
+                in.close();
+            } else {
+                createProperties();
+            }
+            properties.setProperty(propertyKey, propertyValue);
+            // create an output stream
+            FileOutputStream out = new FileOutputStream
+                    (basePath + PROPERTIES_PATH + PROPERTIES_FILE_NAME);
+            properties.storeToXML(out, null, StandardCharsets.ISO_8859_1);
+            out.close();
+            return true;
+        } catch (Exception e) {
+            System.err.println("Error while setting a property: " + e.getMessage());
+            return false;
+        }
     }
 }
