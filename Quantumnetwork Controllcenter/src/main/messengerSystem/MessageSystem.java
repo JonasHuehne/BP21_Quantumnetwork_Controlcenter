6dab6e0221015a2f154810e58d760b89a878f4b3
sashapetri <sasha.petri@stud.tu-darmstadt.de>
2022-03-11 01:17:19 +0100

    basics for sending encrypted text messages and files
    sending unverified messages already works in unit tests, but still more
    bugs to fix etc.

--- Quantumnetwork Controllcenter/src/main/messengerSystem/MessageSystem.java
+++ Quantumnetwork Controllcenter/src/main/messengerSystem/MessageSystem.java
@@ -1,27 +1,29 @@
 package messengerSystem;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
 import java.sql.SQLException;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.LinkedList;
-import encryptionDecryption.AES256;
+import java.util.Random;
+
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
+
+import encryptionDecryption.FileCrypter;
 import encryptionDecryption.SymmetricCipher;
+import exceptions.CouldNotSendMessageException;
 import exceptions.EndpointIsNotConnectedException;
 import exceptions.ManagerHasNoSuchEndpointException;
 import exceptions.NoKeyForContactException;
 import exceptions.NotEnoughKeyLeftException;
-
-import java.util.Random;
-
 import frame.Configuration;
-import frame.QuantumnetworkControllcenter;
 import keyStore.SimpleKeyStore;
 import networkConnection.ConnectionEndpoint;
 import networkConnection.ConnectionManager;
-import networkConnection.ConnectionState;
 import networkConnection.MessageArgs;
 import networkConnection.NetworkPackage;
 import networkConnection.TransmissionTypeEnum;
@@ -73,74 +75,172 @@ public class MessageSystem {
 	}
 	
 	/**
-	 * Sends a text message.
+	 * Constructs a NetworkPackage with the given parameters and sends it to the specified partner. <br>
+	 * Signs the NetworkPackage with the authenticator of this class if desired.
 	 * @param connectionID
-	 * 		the ID of the {@linkplain ConnectionEndpoint} that is to send a message to its partner <br>
-	 * 		must be a {@linkplain ConnectionEndpoint} in the {@linkplain ConnectionManager} of this class
-	 * @param msgString
-	 * 		the text message to send
+	 * 		connectionID of a {@linkplain ConnectionEndpoint} in the {@linkplain ConnectionManager} of this class <br>
+	 * 		the constructed NetworkPackage is sent to the connected partner of the specified endpoint
+	 * @param type
+	 * 		type of the NetworkPackage
+	 * @param args
+	 * 		arguments of the NetworkPackage
+	 * @param content
+	 * 		content of the network package
 	 * @param sign
-	 * 		whether the message should be signed
+	 * 		whether to sign the NetworkPackage
 	 * @param confirm
-	 * 		whether the recipient should send back a confirmation message ({@linkplain TransmissionTypeEnum#RECEPTION_CONFIRMATION} if they receive the message 
-	 * @param keyIndex
-	 * 		if this is not an encrypted message, set this to -1 <br>
-	 * 		if this is an encrypted message this is the index at which the used section of the key 
-	 * 		between the sender of the message and the recipient starts,
-	 * 		allowing the recipient to know which bits are to be used for decryption
+	 * 		whether the recipient should confirm that they received the package
 	 * @throws EndpointIsNotConnectedException
-	 * 		if the specified connection endpoint is not connected to their partner
-	 * @throws ManagerHasNoSuchEndpointException 
+	 * 		if the specified endpoint is not connected to their partner
+	 * @throws ManagerHasNoSuchEndpointException
+	 * 		if no endpoint of the given name exists in the ConnectionManager of this class
 	 */
-	public static void sendTextMessage(String connectionID, String msgString, boolean sign, boolean confirm, int keyIndex) throws EndpointIsNotConnectedException, ManagerHasNoSuchEndpointException {
-		MessageArgs args = new MessageArgs(keyIndex);
-		TransmissionTypeEnum type = TransmissionTypeEnum.TEXT_MESSAGE;
-		byte[] messageBytes = stringToByteArray(msgString);
-		NetworkPackage message = new NetworkPackage(type, args, messageBytes, confirm);
+	private static void sendMessage(String connectionID, TransmissionTypeEnum type, MessageArgs args, byte[] content, boolean sign, boolean confirm)
+			throws EndpointIsNotConnectedException, ManagerHasNoSuchEndpointException {
+		NetworkPackage message = new NetworkPackage(type, args, content, confirm);
 		if (sign) message.sign(authenticator);
 		conMan.sendMessage(connectionID, message);
 	}
 	
 	/**
-	 * Sends a file.
+	 * 
 	 * @param connectionID
-	 * 		the ID of the {@linkplain ConnectionEndpoint} that is to send a message to its partner <br>
-	 * 		must be a {@linkplain ConnectionEndpoint} in the {@linkplain ConnectionManager} of this class
+	 * @param file
+	 * @param encryptFile
+	 * @param sign
+	 * @param confirm
+	 * @throws CouldNotSendMessageException
+	 * 		if the file could not be sent <br>
+	 * 		wraps a lower level exception, such as IOException or {@linkplain EndpointIsNotConnectedException}
+	 */
+	private static void sendFileInternal(String connectionID, File file, boolean encryptFile, boolean sign, boolean confirm) throws CouldNotSendMessageException {
+		byte[] fileBytes;
+		int keyIndex;
+		
+		try {
+			if (encryptFile) { // sending an encrypted file
+				informPartnerAboutKeyUse(); // inform partner for key synchronization purposes
+				// encrypt the file locally
+				byte[] byteKey = SimpleKeyStore.getKeyBytes(connectionID, cipher.getKeyLength());
+				keyIndex = SimpleKeyStore.getIndex(connectionID);
+				SecretKey key = cipher.byteArrayToSecretKey(byteKey);
+				Path pathToEncryptedFile = Paths.get(file.getParent().toString(), "encrypted_" + file.getName());
+				FileCrypter.encryptAndSave(file, cipher, key, pathToEncryptedFile);
+				// read the encrypted file to send
+				fileBytes = Files.readAllBytes(pathToEncryptedFile);
+			} else {
+				// read the file to send
+				fileBytes = Files.readAllBytes(file.toPath());
+				keyIndex = -1; // indicates file isn't encrypted
+			}
+			
+			/*
+			 * For large files, it may (?) be beneficial to split it into multiple packages. 
+			 * However, this would be a significantly more complicated protocol to prevent package loss etc.
+			 */
+			
+			MessageArgs args = new MessageArgs(file.getName(), keyIndex);
+			sendMessage(connectionID, TransmissionTypeEnum.FILE_TRANSFER, args, fileBytes, (sign || encryptFile), confirm);
+			
+		} catch (EndpointIsNotConnectedException | ManagerHasNoSuchEndpointException | IOException | InvalidKeyException | 
+				IllegalBlockSizeException | SQLException | NotEnoughKeyLeftException | NoKeyForContactException e) {
+			throw new CouldNotSendMessageException("Could not send the file " + file.getName() + " along the connection " + connectionID + ".", e);
+		}
+
+	}
+	
+	/**
+	 * Sends an unencrypted text message to the specified communication partner.
+	 * @param connectionID
+	 * 		connectionID of a {@linkplain ConnectionEndpoint} in the {@linkplain ConnectionManager} of this class <br>
+	 * 		the text message is sent to the connected partner of the specified endpoint
+	 * @param msgString
+	 * 		the message to send
+	 * @param sign
+	 * 		true if the message should be signed, false if not
+	 * @param confirm
+	 * 		set this to true if the recipient should send a confirmation message upon receiving the message, false otherwise
+	 * @throws CouldNotSendMessageException
+	 * 		if the message could not be sent <br>
+	 * 		wraps a lower level exception, such as IOException or {@linkplain EndpointIsNotConnectedException}
+	 */
+	public static void sendTextMessage(String connectionID, String msgString, boolean sign, boolean confirm) throws CouldNotSendMessageException  {
+		MessageArgs args = new MessageArgs();
+		try {
+			sendMessage(connectionID, TransmissionTypeEnum.TEXT_MESSAGE, args, stringToByteArray(msgString), sign, confirm);
+		} catch (EndpointIsNotConnectedException | ManagerHasNoSuchEndpointException e) {
+			throw new CouldNotSendMessageException("Could not send the specified message along the connection " + connectionID + ".", e);
+		}
+	}
+	
+	/**
+	 * Encrypts a text message and sends it to the specified communication partner.
+	 * @param connectionID
+	 * 		connectionID of a {@linkplain ConnectionEndpoint} in the {@linkplain ConnectionManager} of this class <br>
+	 * 		the text message is sent to the connected partner of the specified endpoint
+	 * @param msgString
+	 * @param confirm
+	 * 		set this to true if the recipient should send a confirmation message upon receiving the message, false otherwise
+	 * @throws CouldNotSendMessageException
+	 * 		if the message could not be sent, or an error during encryption occured <br>
+	 * 		wraps a lower level exception, such as IOException or {@linkplain EndpointIsNotConnectedException}
+	 */
+	public static void sendEncryptedTextMessage(String connectionID, String msgString, boolean confirm) throws CouldNotSendMessageException {
+		try {
+			// inform partner that you wish to send an encrypted message (to prevent key desync)
+			informPartnerAboutKeyUse();
+			// encrypt the message
+			byte[] msgBytes = stringToByteArray(msgString);
+			byte[] key = SimpleKeyStore.getKeyBytes(msgString, cipher.getKeyLength());
+			byte[] encMsgBytes = cipher.encrypt(msgBytes, key);
+			// Provide the index in the message args so receiver knows where to start with decryption
+			int index = SimpleKeyStore.getIndex(connectionID);
+			MessageArgs args = new MessageArgs(index);
+			sendMessage(connectionID, TransmissionTypeEnum.TEXT_MESSAGE, args, encMsgBytes, true, confirm);
+		} catch (EndpointIsNotConnectedException | ManagerHasNoSuchEndpointException | 
+			SQLException | NotEnoughKeyLeftException | NoKeyForContactException | InvalidKeyException | IllegalBlockSizeException e) {
+			throw new CouldNotSendMessageException("Could not send the encrypted message along the connection " + connectionID + ".", e);
+		}
+	}
+	
+	/**
+	 * Sends an unencrypted file to the specified communication partner.
+	 * @param connectionID
+	 * 		connectionID of a {@linkplain ConnectionEndpoint} in the {@linkplain ConnectionManager} of this class <br>
+	 * 		the file is sent to the connected partner of the specified endpoint
 	 * @param file
 	 * 		the file to send
 	 * @param sign
-	 * 		whether the message should be signed
+	 * 		true if the message should be signed, false if not
 	 * @param confirm
-	 * 		whether the recipient should send back a confirmation message ({@linkplain TransmissionTypeEnum#RECEPTION_CONFIRMATION} if they receive the message 
-	 * @param keyIndex
-	 * 		if this is not an encrypted message, set this to -1 <br>
-	 * 		if this is an encrypted message this is the index at which the used section of the key 
-	 * 		between the sender of the message and the recipient starts,
-	 * 		allowing the recipient to know which bits are to be used for decryption 
-	 * @throws EndpointIsNotConnectedException
-	 * 		if the specified connection endpoint is not connected to their partner
-	 * @throws ManagerHasNoSuchEndpointException 
+	 * 		set this to true if the recipient should send a confirmation message upon receiving the file, false otherwise
+	 * @throws CouldNotSendMessageException
+	 * 		if the file could not be sent <br>
+	 * 		wraps a lower level exception, such as IOException or {@linkplain EndpointIsNotConnectedException} 
 	 */
-	public static void sendFile(String connectionID, File file, boolean sign, boolean confirm, int keyIndex) throws EndpointIsNotConnectedException, ManagerHasNoSuchEndpointException {
-		String fileName = file.getName();
-		TransmissionTypeEnum type = TransmissionTypeEnum.FILE_TRANSFER;
-		MessageArgs args = new MessageArgs(fileName, keyIndex);
-		byte[] messageBytes = null; // TODO load file here
-		NetworkPackage message = new NetworkPackage(type, args, messageBytes, confirm);
-		if (sign) message.sign(authenticator);
-		conMan.sendMessage(connectionID, message);
+	public static void sendFile(String connectionID, File file, boolean sign, boolean confirm) throws CouldNotSendMessageException {
+		sendFileInternal(connectionID, file, false, sign, confirm);
 	}
 	
-	/*
-	 * TODO: one method each for sending encrypted and non-encrypted text messages, then make the two methods above private
-	 * same for files (this prevents erronous calls / desync errors based on programmers being inexperienced or confused)
+	/**
+	 * Encrypts a file and sends it to the specified communication partner.
+	 * @param connectionID
+	 * 		connectionID of a {@linkplain ConnectionEndpoint} in the {@linkplain ConnectionManager} of this class <br>
+	 * 		the file is sent to the connected partner of the specified endpoint
+	 * @param file
+	 * 		the file to encrypt and send
+	 * @param confirm
+	 * 		set this to true if the recipient should send a confirmation message upon receiving the file, false otherwise
+	 * @throws CouldNotSendMessageException
+	 * 		if the file could not be sent, or an error during encryption occured <br>
+	 * 		wraps a lower level exception, such as IOException or {@linkplain EndpointIsNotConnectedException} 
 	 */
+	public static void sendEncryptedFile(String connectionID, File file, boolean confirm) throws CouldNotSendMessageException {
+		sendFileInternal(connectionID, file, true, true, confirm);
+	}
 	
-	
-	private static void sendEncryptedFile() {
-		// Encrpyt file locally
-		
-		// Send the file as a regular message, with a flag that the contents are encrypted
+	private static void informPartnerAboutKeyUse() {
+		// TODO
 	}
 	
 	/**Utility for converting a byte[] to a String.
