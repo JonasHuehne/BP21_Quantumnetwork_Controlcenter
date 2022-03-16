a9a5d1504d84af76295e960329f501d96347b233
sarah <sarah@munin-soft.de>
2022-03-16 23:19:18 +0100

    external API now compiles, still untested etc. (see details)
    i tried to preserve the original function of the class as much as
    possible, however, there were some issues such as the export key method
    returning the full key, but the ciphers only using a certain byte lenght
    etc, which showed that the class was rather dated
    
    I have tried to preserve the original as much as I could while at the
    same time implementing what was specified in the US for Externe API
    
    rebase

--- Quantumnetwork Controllcenter/src/main/externalAPI/ExternalAPI.java
+++ Quantumnetwork Controllcenter/src/main/externalAPI/ExternalAPI.java
@@ -1,47 +1,101 @@
 package externalAPI;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map.Entry;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.SecretKey;
 
-import exceptions.EndpointIsNotConnectedException;
+import communicationList.CommunicationList;
+import communicationList.SQLiteCommunicationList;
+import encryptionDecryption.AES256;
+import encryptionDecryption.FileCrypter;
+import encryptionDecryption.SymmetricCipher;
+import exceptions.ConnectionAlreadyExistsException;
+import exceptions.CouldNotDecryptMessageException;
+import exceptions.CouldNotEncryptMessageException;
+import exceptions.CouldNotGetKeyException;
+import exceptions.CouldNotSendMessageException;
+import exceptions.IpAndPortAlreadyInUseException;
 import exceptions.ManagerHasNoSuchEndpointException;
+import exceptions.NoKeyWithThatIDException;
+import exceptions.NotEnoughKeyLeftException;
+import exceptions.PortIsInUseException;
 import frame.Configuration;
 import keyStore.KeyStoreDbManager;
+import messengerSystem.MessageSystem;
+import messengerSystem.SHA256withRSAAuthentication;
+import messengerSystem.SignatureAuthentication;
+import networkConnection.ConnectionEndpoint;
+import networkConnection.ConnectionManager;
 
 /**
  * An API class for external use to get keys and use the encryption/decryption with or without sending the file.
- * 
- * @author Lukas Dentler
+ * @implNote The API should be functional, but is still very underdeveloped. 
+ * @author Lukas Dentler, Sasha Petri
  */
 public class ExternalAPI {
 	
+	private static boolean initialized = true;
+	private static ConnectionManager conMan; // connection manager, used when joining the network
+	private static SymmetricCipher cipher; // cipher used for encryption / decryption
+	
+
 	/**
-	 * returning a key for the stated communication partner
-	 * 
-	 * if "42debugging42" is used as parameter for communicationPartner a fixed key is returned for debugging and testing purposes
-	 * 
-	 * @param communicationPartner name of the receiver as saved in communicationList 
-	 * @return a key as byte array
+	 * Gets a number of key bytes for the specified communicationPartner from the keyStore.
+	 * @param communicationPartner
+	 * 		the partner to get key bytes for <br>
+	 * 		must have an entry in the key store with an id that is equal to this name
+	 * @param nbytes
+	 * 		the amount of key bytes to get
+	 * @param index
+	 * 		the index at which to start getting key bytes from
+	 * @return
+	 * 		{@code key[index]} to {@code key[index + n]}, where {@code key} is the key saved with id {@code communicationPartner}
+	 * @throws CouldNotGetKeyException 
+	 * 		if the specified key could not be retrieved
+	 * @see KeyStoreDbManager#getKeyBytesAtIndexN(String, int, int)
 	 */
-	public static byte[] exportKeyByteArray(String communicationPartner) {
-		//key for debugging purposes
-		if(communicationPartner.equals("42debugging42")) {
-			return new byte[] { (byte) 1, (byte) 2, (byte) 3, (byte) 4, (byte) 5, (byte) 6, (byte) 7, (byte) 8, (byte) 9, (byte) 10, (byte) 11, (byte) 12, (byte) 13, (byte) 14, (byte) 15, (byte) 16, (byte) 17, (byte) 18, (byte) 19, (byte) 20, (byte) 21, (byte) 22, (byte) 23, (byte) 24, (byte) 25, (byte) 26, (byte) 27, (byte) 28, (byte) 29, (byte) 30, (byte) 31, (byte) 32}; 
+	public static byte[] exportKeyByteArray(String communicationPartner, int nbytes, int index) throws CouldNotGetKeyException {
+		try {
+			return KeyStoreDbManager.getKeyBytesAtIndexN(communicationPartner, nbytes, index);
+		} catch (NotEnoughKeyLeftException | NoKeyWithThatIDException | SQLException e) {
+			throw new CouldNotGetKeyException("Could not get the specified key for communication partner " + communicationPartner , e);
 		}
-		return KeyStoreDbManager.getEntryFromKeyStore(communicationPartner).getCompleteKeyBuffer();
 	}
 	
 	/**
-	 * returning a key for the stated communication partner
-	 * 
-	 * @param communicationPartner name of the receiver as saved in communicationList 
-	 * @return a key as String
-	 */
-	public static String exportKeyString(String communicationPartner) {
-		byte[] keyBytes = exportKeyByteArray(communicationPartner);
+	 * Gets a number of key bytes for the specified communicationPartner from the keyStore
+	 * and then gives a string consisting of these bytes.
+	 * @param communicationPartner
+	 * 		the partner to get key bytes for <br>
+	 * 		must have an entry in the key store with an id that is equal to this name
+	 * @param nbytes
+	 * 		the amount of key bytes to get
+	 * @param index
+	 * 		the index at which to start getting key bytes from
+	 * @return
+	 * 		{@code key[index]} to {@code key[index + n]}, where {@code key} is the key saved with id {@code communicationPartner}
+	 * @throws CouldNotGetKeyException 
+	 * 		if the specified key could not be retrieved
+	 * @see KeyStoreDbManager#getKeyBytesAtIndexN(String, int, int)
+	 */ // <Sasha> not sure where this is useful
+	public static String exportKeyString(String communicationPartner, int nbytes, int index) throws CouldNotGetKeyException {
+		byte[] keyBytes = exportKeyByteArray(communicationPartner, nbytes, index);
 		StringBuilder keyString = new StringBuilder();
 		
 		for(int i = 0; i < keyBytes.length; i++) {
@@ -52,13 +106,22 @@ public class ExternalAPI {
 	}
 	
 	/**
-	 * returning a key for the stated communication partner
+	 * Gets a key ready to be used by the {@link #cipher} used in this API. <br>
+	 * Key will be gotten from the keystore entry for the specified 
+	 * communication partner at the given index.
 	 * 
-	 * @param communicationPartner name of the receiver as saved in communicationList 
-	 * @return a key as SecretKey object
+	 * @param communicationPartner
+	 * 		the partner to get key bytes for <br>
+	 * 		must have an entry in the key store with an id that is equal to this name
+	 * @return a key as a SecretKey Object <br>
+	 * key will be {@code key[index]} to {@code key[index + n]}, 
+	 * where {@code key} is the key saved with id {@code communicationPartner}
+	 * and {@code n} is the key length of the currently used cipher in bytes
+	 * @throws CouldNotGetKeyException 
+	 * 		if the specified key could not be retrieved
 	 */
-	public static SecretKey exportKeySecretKey(String communicationPartner) {
-		return encryptionDecryption.CryptoUtility.byteArrayToSecretKeyAES256(exportKeyByteArray(communicationPartner));
+	public static SecretKey exportKeySecretKey(String communicationPartner, int index) throws CouldNotGetKeyException {
+		return cipher.byteArrayToSecretKey(exportKeyByteArray(communicationPartner, cipher.getKeyLength() / 8, index));
 	}
 	
 	/**
@@ -77,66 +140,171 @@ public class ExternalAPI {
 	}
 	
 	/**
-	 * method to encrypt a given file in the externalAPI directory
-	 * 
-	 * @param communicationPartner name of the receiver as saved in communicationList 
-	 * @param fileName name of the file to be encrypted
+	 * Encrypts a file using the next key bytes saved for a specified contact.
+	 * File will be saved in the external API directory. The file name of the encrypted
+	 * file will include the index at which the key starts that was used for encryption.
+	 * @param communicationPartner
+	 * 		whose key the file is to be encrypted with <br>
+	 * 		this method will retrieve a key from the keystore saved under this ID
+	 * @param inFile
+	 * 		the file to encrypt 
+	 * @throws CouldNotEncryptMessageException 
+	 * 		if the file could not be encrypted, wraps another Exception
 	 */
-	public static void encryptFile(String communicationPartner, String fileName) {
-		Path externalPath = getExternalAPIPath();
-		Path toEncrypt = externalPath.resolve(fileName);
-		
-		byte[] key = exportKeyByteArray(communicationPartner);
-		
-		if(Files.exists(toEncrypt)) {
-			File encrypt = toEncrypt.toFile();
-			File encrypted = new File(externalPath.resolve("encrypted_" + fileName).toString());
-			encryptionDecryption.AES256.encryptFile(encrypt, key, encrypted);
-		}
-		else {
-			System.err.println("Error, could not find the file to encrypt, expected" + toEncrypt.normalize());
+	public static void encryptFile(String communicationPartner, File inFile) throws CouldNotGetKeyException, CouldNotEncryptMessageException {
+		try {
+			int index = KeyStoreDbManager.getIndex(communicationPartner);
+			SecretKey sk = exportKeySecretKey(communicationPartner, index);
+			Path outPath = Paths.get(getExternalAPIPath().toString(), "encr_index_" + index + "_" + inFile.getName());
+			
+			FileCrypter.encryptAndSave(inFile, cipher, sk, outPath);
+		} catch (CouldNotGetKeyException | InvalidKeyException | IllegalBlockSizeException | IOException | NoKeyWithThatIDException | SQLException e) {
+			throw new CouldNotEncryptMessageException("Could not encrypt the file " + inFile + " with key of " + communicationPartner, e);
 		}
 	}
 	
 	/**
-	 * method to decrypt a given file in the externalAPI directory
-	 * 
-	 * @param communicationPartner name of the receiver as saved in communicationList
-	 * @param fileName name of the file to be decrypted
-	 */
-	public static void decryptFile(String communicationPartner, String fileName) {
-		Path externalPath = getExternalAPIPath();
-		Path toDecrypt = externalPath.resolve(fileName);
-		
-		byte[] key = exportKeyByteArray(communicationPartner);
-		
-		if(Files.exists(toDecrypt)) {
-			File decrypt = toDecrypt.toFile();
-			File decrypted = new File(externalPath.resolve("decrypted_" + fileName).toString());
-			encryptionDecryption.AES256.decryptFile(decrypt, key, decrypted);
+	 * Decrypts a file using the key saved for the specified ID.
+	 * Uses the bytes starting at the given index in the keystore entry.
+	 * @param communicationPartner
+	 * 		who sent the files <br>
+	 * 		this method will retrieve a key from the keystore saved under this ID
+	 * @param index
+	 * 		the bytes to be used for decryption will be read starting at that
+	 * 		index in the keystore (needs to be the same as the index was when
+	 * 		the file was decrypted with the same key)
+	 * @param file
+	 * 		the file to decrypt
+	 * @throws CouldNotDecryptMessageException 
+	 * 		if the file could not be decrypted
+	 * @see KeyStoreDbManager
+	 */ // Generally, it will not be necessary to call this method (decryption of files is automatic in the message system)
+	public static void decryptFile(String communicationPartner, int index, File inFile) throws CouldNotDecryptMessageException {
+		try {
+			byte[] key = KeyStoreDbManager.getKeyBytesAtIndexN(communicationPartner, cipher.getKeyLength() / 8, index);
+			SecretKey sk = cipher.byteArrayToSecretKey(key);
+			Path outPath = Paths.get(getExternalAPIPath().toString(), "decr_" + inFile.getName());
+			
+			FileCrypter.decryptAndSave(inFile, cipher, sk, outPath);
+		} catch ( NotEnoughKeyLeftException | NoKeyWithThatIDException | SQLException | InvalidKeyException | BadPaddingException | IOException e) {
+			throw new CouldNotDecryptMessageException("Could not decrypt the file " + inFile + " with key of " + communicationPartner + " starting at index " + index, e);
 		}
-		else {
-			System.err.println("Error, could not find the file to decrypt, expected" + toDecrypt.normalize());
-		}
-		
-		/*
-		 * TODO: maybe rework this function to use with a given key index or something similar to reduce unwanted effects
-		 * when using this message and communicating over the messengerSystem in close temporal proximity.
-		 */
 	}
 	
 	/**
-	 * @deprecated This is changed in the US for sending and receiving encrypted files.
+	 * Encrypts the contents of a .txt file and sends them as a text message to the specified partner.
+	 * Requires externalAPI to be initialized.
+	 * @param communicationPartner 
+	 * 	ID of the partner to send the message to <br>
+	 *	because we are sending an encrypted message, this partner needs a key in the keystore (saved under their ID)
+	 * @param file
+	 * 	the file to send, must be a .txt file
+	 * @throws CouldNotSendMessageException
+	 * 	if the message could not be sent, e.g. because there is no key available, or the file could not be read
 	 */
-	public static boolean sendEncryptedTxtFile(String communicationPartner, String fileName) {
-		return false;
+	public static void sendEncryptedTxtFile(String communicationPartner, File file) throws ManagerHasNoSuchEndpointException, CouldNotSendMessageException {
+		if (!file.toString().endsWith(".txt")) {
+			throw new IllegalArgumentException("File " + file.toString() + " is not a .txt file.");
+		}
+		if (!initialized) return;
+		try {
+			// load the text of the file
+			String msg = Files.readString(file.toPath());
+			// send it as an encrypted message
+			MessageSystem.sendEncryptedTextMessage(communicationPartner, msg, false);
+		} catch (IOException e) {
+			throw new CouldNotSendMessageException("Could not send the encrypted txt file - an I/O Exception occured.", e);
+		}
 	}
 	
 	/**
-	 * @deprecated This is changed in the US for sending and receiving encrypted files.
+	 * @return all messages received on this node in the network <br>
+	 * each entry in the list is of the form (i, msg) where i is the ID of the CE on which the message was received
+	 * and msg is the message itself
 	 */
-	public static void receiveEncryptedTxtFile(String communicationPartner) {
-
+	public static ArrayList<SimpleEntry<String, String>> getAllReceivedMessages() {
+		if (!initialized) return null;
+		
+		ArrayList<SimpleEntry<String, String>> receivedMessages = new ArrayList<SimpleEntry<String, String>>();
+		for (Entry<String, ConnectionEndpoint> e : conMan.returnAllConnections().entrySet()) {
+			for (Entry<String, String> chatMessage : e.getValue().getChatLog()) { // for each CE in the CM, get the chatlog
+				if (chatMessage.getKey().contains(e.getValue().getRemoteName())) { // if the message came from the other party
+					receivedMessages.add(new SimpleEntry<>(e.getValue().getID(), chatMessage.getValue())); // add it to the list of received messages
+				}
+			}
+		}
+		
+		return receivedMessages;
+	}
+	
+	/**
+	 * Writes all received messages to a txt file.
+	 * @throws IOException 
+	 * 		if there was an issue with writing the files
+	 */
+	public static void writeReceivedMessagesToTXT() throws IOException { // replaces receiveEncryptedTxtFile of the old implementation
+		ArrayList<SimpleEntry<String, String>> messages = getAllReceivedMessages();
+		if (messages == null) return;
+		
+		Date date = Calendar.getInstance().getTime();  
+		String dateString = (new SimpleDateFormat("yyyy-mm-dd hh-mm-ss")).format(date);
+		Path outpath = Paths.get(Configuration.getBaseDirPath(), "externalAPI", "receivedTexts", dateString + ".txt");
+		try (BufferedWriter buffer = new BufferedWriter(new FileWriter(outpath.toFile()));) {
+			for (SimpleEntry<String, String> message : messages) {
+				buffer.write("Received Message [" + message.getValue() + "] on CE [" + message.getKey() + "]" );
+			}
+		}
+	}
+	
+	/**
+	 * Joins the Peer-to-Peer Network as a member with the given name, IP and port.
+	 * Needed to send and receive messages.
+	 * @param localName
+	 * 		the name to join the network with
+	 * @param localAddress
+	 * 		the local address to join the network with
+	 * @param localPort
+	 * 		local server port that other members will connect to
+	 * @throws IOException
+	 * 		may occur if there was an I/O error while initializing the ConnectionManager
+	 * @throws PortIsInUseException
+	 * 		if a ConnectionManager is running on this machine already using that port
+	 */
+	public static void initialize(String localName, String localAddress, int localPort) throws IOException, PortIsInUseException {
+		if (initialized) return; // do not initialize twice
+		
+		CommunicationList commList = new SQLiteCommunicationList();
+		conMan = new ConnectionManager(localAddress, localPort, localName, commList);
+		
+		SignatureAuthentication auth = new SHA256withRSAAuthentication();
+		// if there is no (sk, pk) pair create it
+		// TODO (easier once Sarah's PR is done)
+		cipher = new AES256();
+		MessageSystem.setAuthenticationAlgorithm(auth);
+		MessageSystem.setEncryption(cipher);
+		
+		initialized = true;
+	}
+	
+	
+	/**
+	 * Connects to another endpoint in the network.
+	 * @param ip
+	 * 		ip of the endpoint to connect to
+	 * @param port
+	 * 		port of the endpoint to connect to
+	 * @param name
+	 * 		name (ID) under which to save the endpoint, later used for sending messages 
+	 * @param pk
+	 * 		pk of the endpoint, required for validating messages from that endpoint
+	 * @throws IpAndPortAlreadyInUseException 
+	 * 		if a connection with this IP:Port pair already exists 
+	 * @throws ConnectionAlreadyExistsException 
+	 * 		if a connection with this ID already exists
+	 */
+	public static void connectTo(String ip, int port, String name, String pk) throws ConnectionAlreadyExistsException, IpAndPortAlreadyInUseException {
+		if (!initialized) return;
+		conMan.createNewConnectionEndpoint(name, ip, port, pk);
 	}
 	
 	/*
